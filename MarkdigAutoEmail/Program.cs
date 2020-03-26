7203c6c8aa3a0b5d7aa49fe79ead52994bab7485
Ham Vocke <hvocke@stackoverflow.com>
2020-03-26 17:47:59 +0100

    add missing `Program.cs`

---
+++ MarkdigAutoEmail/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MarkdigAutoEmail
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please run the tests in the test project");
+        }
+    }
+}
